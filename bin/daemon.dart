654b36ea3e83425a76e0a89b8d97d2e252cc2a00
Liam Ryde <liamryde@gmail.com>
2021-05-28 09:27:05 -0400

    Socket frame

--- bin/daemon.dart
+++ bin/daemon.dart
@@ -1,3 +1,30 @@
+import 'dart:developer';
+import 'dart:io';
+
+final String HOST = "localhost";
+final int PORT = 15744;
+
 void main(List<String> arguments) {
-  print('Hello world!');
+  var webSocketHandler = WebSocketHandler();
+  print('brunch-tools-daemon: Attempted to bind to '+HOST+':'+PORT.toString()+'!');
+  HttpServer.bind(HOST, PORT).then((server) => {
+    server.listen((request) {
+      print('brunch-tools-daemon: Bound, listening to websocket requests.');
+      WebSocketTransformer.upgrade(request).then((socket) => {
+        webSocketHandler.newRequest(socket)
+      });
+    })
+  });
+}
+
+class WebSocketHandler {
+  Map<String, WebSocket> _users = {};
+
+  void newRequest(WebSocket socket) {
+    log('Incoming connection ${socket.hashCode}');
+    socket.listen((event) {
+
+    });
+  }
+
 }
